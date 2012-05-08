513bc88504f84ab800b093161f4d4dfd44160875
Eric Bréchemier <scopeornot@eric.brechemier.name>
2012-05-08 16:57:50 +0200

    Define aliases instead of using global variables in run-code-with-needs.js

--- scripts/run-code-with-needs.js
+++ scripts/run-code-with-needs.js
@@ -1,9 +1,16 @@
 scope(function(context){
+
+  var
+    // declare aliases
+    Number = context.Number,
+    Date = context.Date,
+    log = context.log;
+
   function timestamp(){
     return Number(new Date());
   }
 
-  if (context.log){
-    context.log( "Code has run at timestamp: "+timestamp() );
+  if (log){
+    log( "Code has run at timestamp: "+timestamp() );
   }
-},["log"]);
+},["log","Number","Date"]);
