1da291d29af49586db4cf4182b24e943e3a1786c
Eric Bréchemier <bezen@eric.brechemier.name>
2012-07-30 18:28:21 +0200

    Do not import properties from parent context
    I expected this feature to make it easier to work with global
    objects copied in the shared context, but it turned out wrong:
    in the browsers, there are unexpected global properties which
    may have the same name as a module, e.g. "home" which is a
    function to replace the current page with the home page configured
    by the user. In case such a property is found, the dependency
    is never fetched e.g. when using AMD to load it asynchronously
    when needed.
    
    Due to the unexpected result, this feature will be deprecated.
    I moved it to a separate module, scope-level2-shared-imports.js,
    which will be deleted in a future version.

--- scope-level2-shared.js
+++ scope-level2-shared.js
@@ -12,63 +12,6 @@ scope(function(parentContext){
     privateContext = {};
 
   /*
-    Function: importFromParentContext(needs)
-    Copy missing needs found in parent context to private context
-
-    Parameter:
-      needs - optional array of strings, the names of the properties that
-              code would like to find in the private context, defaults to []
-
-    Note:
-    Nothing happens in case needs argument is null or undefined.
-  */
-  function importFromParentContext(needs){
-    needs = needs || [];
-
-    var
-      i,
-      need;
-
-    // for each need
-    for (i=0; i<needs.length; i++){
-      need = needs[i];
-      if (
-        // not found in private context and found in parent context
-        !privateContext.hasOwnProperty(need) && need in parentContext
-      ){
-        // copy from parent context to private context
-        privateContext[need] = parentContext[need];
-      }
-    }
-  }
-
-  /*
-    Function: exportToPrivateContext(name,value)
-    Set the value of a property with given name in private context
-
-    Parameters:
-      name - string, the name of the property to set
-      value - any, the value of the property
-
-    Notes:
-    1. Nothing happens in case the given name is not a string.
-
-    2. If the name is "scope", the value is copied both in private context
-    and in the parent context to replace the global scope() function.
-  */
-  function exportToPrivateContext(name,value){
-    if (typeof name !== "string"){
-      return;
-    }
-
-    privateContext[name] = value;
-    if (name === "scope"){
-      // replace the current implementation of scope() in parent context
-      parentContext.scope = value;
-    }
-  }
-
-  /*
     Function: scope(code[,needs[,name]]): any
     Run code immediately in a private context, and set the return value,
     if any, to a property with given name in the private context
@@ -83,25 +26,19 @@ scope(function(parentContext){
 
     Returns:
       any, the return value of the code
-
-    Note:
-    This implementation is expected to be defined on top of scope-bootstrap.
-    It replaces the old implementation of scope() that was used for its own
-    definition.
-
-    If a requested property is missing in the private context but present in
-    the parent context, the property is copied from the parent context to the
-    private context beforehand. The intent is to make global objects available
-    in the private context, while preserving the global context.
-
-    The only exception is the property "scope", which will be copied from the
-    private context to the parent context, to allow the replacement of current
-    implementation of scope() with another implementation defined on top.
   */
   function scope(code,needs,name){
-    importFromParentContext(needs);
     var result = code(privateContext);
-    exportToPrivateContext(name,result);
+
+    if (typeof name !== "string"){
+      return result;
+    }
+
+    privateContext[name] = value;
+    if (name === "scope"){
+      // replace the current implementation of scope() in parent context
+      parentContext.scope = value;
+    }
     return result;
   }
 
