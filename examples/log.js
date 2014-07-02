01b8a6aec8a9477147224b68ba6a7170f9d37ff4
Eric Bréchemier <eric@bezen.org>
2014-07-02 17:10:35 +0200

    Add log module to examples (from EXAMPLES section of README)

---
+++ examples/log.js
@@ -0,0 +1,12 @@
+scope(function(context){
+  var
+    timestamp = context.timestamp,
+    console = context.console;
+
+  function log( message ) {
+    console.log( timestamp(), ": ", message );
+  }
+
+  return log;
+},["console","timestamp"],"log");
+
