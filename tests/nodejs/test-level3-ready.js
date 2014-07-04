bdd5f33852586a538c7d43bdb6c433c2e7824636
Eric Bréchemier <eric@bezen.org>
2014-07-04 10:11:29 +0200

    Test scope-level3-ready both in browser and Node.js

---
+++ tests/nodejs/test-level3-ready.js
@@ -0,0 +1,10 @@
+console.log("Test: scope-level3-ready");
+
+require( '../../scope-level1-global.js' );
+require( '../../scope-level2-shared.js' );
+require( '../../node_modules/assert' );
+require( '../../node_modules/test.js' );
+require( '../../node_modules/console.js' );
+require( './log.js' );
+require( '../../scope-level3-ready.js' );
+require( '../test-scope-level3-ready.js' );
